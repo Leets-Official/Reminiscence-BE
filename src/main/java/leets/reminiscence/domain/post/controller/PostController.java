a631ee9c5335c1f41ff9452552f087708609c240
dhl1924 <dhl1924@naver.com>
2023-08-20 15:26:34 +0900

    fix : controller 수정

--- src/main/java/leets/reminiscence/domain/post/controller/PostController.java
+++ src/main/java/leets/reminiscence/domain/post/controller/PostController.java
@@ -16,7 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/post")
