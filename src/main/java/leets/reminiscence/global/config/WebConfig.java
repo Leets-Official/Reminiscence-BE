5989e25d4fab5c560696f1b6bacf0c5338e7d1ac
dhl1924 <dhl1924@naver.com>
2023-08-22 00:23:24 +0900

    allowOrigins 수정

--- src/main/java/leets/reminiscence/global/config/WebConfig.java
+++ src/main/java/leets/reminiscence/global/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000/", "http://localhost:8080/")
-                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3000);
     }
