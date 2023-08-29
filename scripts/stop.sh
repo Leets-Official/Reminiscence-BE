f178ccbed381ceddea942879e224f93d22ab273d
노정완 <jeongwan.noh@gmail.com>
2023-08-29 16:58:00 +0900

    added PID when stopping service

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -8,6 +8,6 @@ SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 if [ -z "$SERVICE_PID" ]; then
   echo "서비스 NotFound" >> $STOP_LOG
 else
-  echo "서비스 종료 " >> $STOP_LOG
+  echo "서비스 종료 : $SERVICE_PID" >> $STOP_LOG
   kill -9 "$SERVICE_PID"
 fi
