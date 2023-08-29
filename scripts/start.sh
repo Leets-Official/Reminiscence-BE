bd0cd3005e2ac065ead54c190103d6963bf91b2a
노정완 <jeongwan.noh@gmail.com>
2023-08-29 15:38:04 +0900

    fixing CI/CD

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,13 +7,11 @@ APP_LOG="$ROOT_PATH/application.log"
 ERROR_LOG="$ROOT_PATH/error.log"
 START_LOG="$ROOT_PATH/start.log"
 
-NOW=$(date +%c)
+echo "$JAR 복사" >> $START_LOG
+cp $ROOT_PATH/build/libs/reminiscence-0.0.1-SNAPSHOT.jar $JAR
 
-echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/reminiscence-1.0.0.jar $JAR
-
-echo "[$NOW] > $JAR 실행" >> $START_LOG
+echo "$JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $JAR)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+echo "서비스 PID: $SERVICE_PID" >> $START_LOG
