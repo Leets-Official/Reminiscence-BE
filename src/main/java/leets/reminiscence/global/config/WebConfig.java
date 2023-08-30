a0897f1b0032a474ab9991148bc0ddce63f1ed74
dhl1924 <dhl1924@naver.com>
2023-08-30 16:31:19 +0900

    fix : .allowedHeaders 추가

--- src/main/java/leets/reminiscence/global/config/WebConfig.java
+++ src/main/java/leets/reminiscence/global/config/WebConfig.java
@@ -7,11 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(final CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
+                .allowedHeaders("*")
                 .exposedHeaders("Authorization", "Authorization-refresh")
                 .maxAge(3600);
     }
